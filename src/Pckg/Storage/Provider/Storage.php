78fa911c915d2cc9b97f8d6694801661735f62bf
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-08-02 16:46:05 +0200

    Default provider

---
+++ src/Pckg/Storage/Provider/Storage.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pckg\Storage\Provider;
+
+use Pckg\Framework\Config;
+use Pckg\Framework\Provider;
+use Pckg\Storage\Filesystem;
+
+class Storage extends Provider
+{
+
+    public function services()
+    {
+        return [
+            Filesystem::class => function (Config $config) {
+                return new Filesystem($config->get('pckg.storage.source', []), $config->get('pckg.storage.mount', []));
+            }
+        ];
+    }
+}
